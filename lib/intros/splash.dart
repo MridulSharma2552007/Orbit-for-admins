b67af0b6c63ebeae4091387501cbf16c28762c2e
MridulSharma2552007 <mridul25sharma52007@gmail.com>
2025-09-25 20:59:14 +0530

    splash page created

---
+++ lib/intros/splash.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({super.key});
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  double _opacity = 0.0;
+  double _scale = 0.8;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(milliseconds: 500), () {
+      setState(() {
+        _opacity = 1.0;
+        _scale = 1.0;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Center(
+                child: AnimatedScale(
+                  scale: _scale,
+                  duration: const Duration(seconds: 1),
+                  curve: Curves.easeOut,
+                  child: AnimatedOpacity(
+                    opacity: _opacity,
+                    duration: const Duration(seconds: 1),
+                    curve: Curves.easeIn,
+                    child: Image.asset('assets/images/orbit.png', height: 400),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 100.0),
+              child: AnimatedOpacity(
+                opacity: _opacity,
+                duration: const Duration(seconds: 1),
+                curve: Curves.easeIn,
+                child: Text(
+                  'Made by students, for students',
+                  style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black.withOpacity(0.3),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
