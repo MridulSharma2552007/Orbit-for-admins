1e0323cae4010707a5f310fff2ee22d42a327d07
MridulSharma2552007 <mridul25sharma52007@gmail.com>
2025-10-02 21:56:52 +0530

    apis removed

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,9 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Supabase.initialize(
-    url: 'https://noifftnvujdyanqvukmg.supabase.co',
+    url: 'url',
     anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5vaWZmdG52dWpkeWFucXZ1a21nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkyMDMwOTUsImV4cCI6MjA3NDc3OTA5NX0.fkb14Yor9uhvDMzI9Bj5MetN02O32P9050cFPzoGCno',
+        'anonkey',
   );
   Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
